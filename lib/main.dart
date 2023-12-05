d35f6c0065c93aa185a99558b2d2e90b07d715c4
Boghdady <mostafa10020038@gmail.com>
2023-12-06 01:31:29 +0200

    1-Create Article Model 2-make objects to get response 3-make articlelist 4-make for loop to receiving data from api and save it in article model object and add it in article list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:news_app2/screens/home_screen.dart';
+import 'package:news_app2/services/news_services.dart';
 
 void main() {
   runApp(const NewsApp());
+  NewsServices().getGeneralNews();
 }
 
 class NewsApp extends StatelessWidget {
