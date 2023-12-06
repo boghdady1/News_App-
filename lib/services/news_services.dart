8c5867a921030918febf9dbc08c6fc45121ad387
Boghdady <mostafa10020038@gmail.com>
2023-12-06 23:26:19 +0200

    refactor ui

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -6,19 +6,23 @@ class NewsServices {
   final Dio dio = Dio();
 
   Future<List<ArticleModel>> getGeneralNews() async {
-    final Response response = await dio.get(
-        "https://newsapi.org/v2/everything?q=%D9%85%D8%B5%D8%B1&apiKey=a7e0c5e48848418895f871835ea8f3ef&language=ar");
-    Map<String, dynamic> jsonData = response.data;
-    List<dynamic> articles = jsonData["articles"];
-    List<ArticleModel> articlesList = [];
-    for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
-          image: article["urlToImage"],
-          title: article["title"],
-          description: article["description"]);
-      articlesList.add(articleModel);
+    try {
+      final Response response = await dio.get(
+          "https://newsapi.org/v2/everything?q=%D9%85%D8%B5%D8%B1&apiKey=a7e0c5e48848418895f871835ea8f3ef&language=ar");
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData["articles"];
+      List<ArticleModel> articlesList = [];
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+            image: article["urlToImage"],
+            title: article["title"],
+            description: article["description"]);
+        articlesList.add(articleModel);
+      }
+      return articlesList;
+    } on Exception catch (e) {
+      return [];
     }
-    return articlesList;
   }
 }
 // void getHealthNews() async {
--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,42 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:news_app2/models/articles_model.dart';
 import 'package:news_app2/services/news_services.dart';
 
 import 'news_post_sliver_view.dart';
 
-class NewsPostListViewBuilder extends StatefulWidget {
+class NewsPostListViewBuilder extends StatelessWidget {
   const NewsPostListViewBuilder({
     super.key,
   });
 
   @override
-  State<NewsPostListViewBuilder> createState() =>
-      _NewsPostListViewBuilderState();
-}
-
-class _NewsPostListViewBuilderState extends State<NewsPostListViewBuilder> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
-
-  @override
-  initState() {
-    getGeneralNews();
-    super.initState();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsServices().getGeneralNews();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const SliverToBoxAdapter(
-            child: Center(child: CircularProgressIndicator()))
-        : NewsPostListView(
-            articles: articles,
+    return FutureBuilder(
+      future: NewsServices().getGeneralNews(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return NewsPostListView(articles: snapshot.data!);
+        } else if (snapshot.hasError) {
+          return SliverToBoxAdapter(
+            child:
+                Image.asset("assets/thumbnail-medium.jpeg", fit: BoxFit.cover),
           );
+        } else {
+          return const SliverToBoxAdapter(
+              child: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
   }
 }
