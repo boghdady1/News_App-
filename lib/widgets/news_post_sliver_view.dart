d0b291e6bcb3708b2b989babf4231df4d9b35be4
Boghdady <mostafa10020038@gmail.com>
2023-12-06 20:28:35 +0200

    finish api 1-change ui data to api data 2-convert statelessWidget to statefulWidget 3-make future method 4-init state widget to call data from future method to call it when app start before build widget run 5-set state to show data on ui when the data from newsService().getGeneralNews() saved to articles list

--- lib/widgets/news_post_sliver_view.dart
+++ lib/widgets/news_post_sliver_view.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:news_app2/models/articles_model.dart';
+import 'package:news_app2/services/news_services.dart';
 import 'package:news_app2/widgets/news_posts.dart';
 
-class NewsPostListView extends StatelessWidget {
+class NewsPostListView extends StatefulWidget {
   const NewsPostListView({super.key});
+
+  @override
+  State<NewsPostListView> createState() => _NewsPostListViewState();
+}
+
+class _NewsPostListViewState extends State<NewsPostListView> {
+  List<ArticleModel> articles = [];
+
+  @override
+  initState() {
+    getGeneralNews();
+    super.initState();
+  }
+
+  Future<void> getGeneralNews() async {
+    articles = await NewsServices().getGeneralNews();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return SliverList(
         delegate: SliverChildBuilderDelegate(
-      childCount: 10,
+      childCount: articles.length,
       (context, index) {
-        return const NewsPosts();
+        return NewsPosts(
+          articleModel: articles[index],
+        );
       },
     ));
   }
